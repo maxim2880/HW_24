a87ff947bb208736b01177d53c6181d2b4e0d1fb
admin <misuramaxim@gmail.com>
2022-08-16 22:49:02 +0500

    HW

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -8,10 +9,53 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+def do_cmd(cmd, value, data):
+    if cmd == 'filter':
+        result = list(filter(lambda record: value in record, data))
+    elif cmd == 'map':
+        col_num = int(value)
+        result = list(map(lambda record: record.split()[col_num], data))
+    elif cmd == 'unique':
+        result = list(set(data))
+    elif cmd == 'sort':
+        reverse = (value == 'desc')
+        result = sorted(data, reverse=reverse)
+    elif cmd == 'limit':
+        result = data[:int(value)]
+    else:
+        raise BadRequest
+    return result
+
+
+def do_query(params):
+    with open(os.path.join(DATA_DIR, params["file_name"])) as f:
+        file_data = f.readlines()
+
+    res = file_data
+
+    if 'cmd1' in params.keys():
+        res = do_cmd(params['cmd1'], params['value1'], res)
+    if 'cmd2' in params.keys():
+        res = do_cmd(params['cmd2'], params['value2'], res)
+    if 'cmd3' in params.keys():
+        res = do_cmd(params['cmd3'], params['value3'], res)
+
+    return res
+
+
+@app.route("/perform_query", methods=["POST"])
 def perform_query():
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    data = request.json
+    file_name = data["file_name"]
+    if not os.path.exists(os.path.join(DATA_DIR, file_name)):
+        raise BadRequest
+
+    return jsonify(do_query(data))
+
+
+if __name__ == "__main__":
+    app.run()
